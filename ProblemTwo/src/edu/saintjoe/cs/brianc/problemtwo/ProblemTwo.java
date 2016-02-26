93f8c0a0772dbb70ae2c2dafc8d9f8cb8b109072
tcooper <tcooper@localhost>
2016-02-26 16:55:23 -0500

    Added code

--- ProblemTwo/src/edu/saintjoe/cs/brianc/problemtwo/ProblemTwo.java
+++ ProblemTwo/src/edu/saintjoe/cs/brianc/problemtwo/ProblemTwo.java
@@ -3,11 +3,16 @@ package edu.saintjoe.cs.brianc.problemtwo;
 public class ProblemTwo {
 	int first;
 	int second;
+	static int Tyler;
 	String one = "Here we go!!";
 	String two;
 	
 	public int getFirst() {
 		return first;
+	}
+	
+	public static int getTyler() {
+		return Tyler;
 	}
 	
 	public void setFirst(int newValue) {
@@ -19,7 +24,7 @@ public class ProblemTwo {
 	}
 
 	public static void main(String[] args) {
-		// Creat a new ProblemTwo object, reference it with myProblem
+		// Create a new ProblemTwo object, reference it with myProblem
 		ProblemTwo myProblem = new ProblemTwo();
 		
 		// Assign something to the data member named "two"
@@ -27,6 +32,7 @@ public class ProblemTwo {
 		
 		// Prove that it worked by printing the value back out
 		System.out.println("We have '" + myProblem.gettwo() + "' for variable two");
+		System.out.println(getTyler() + " will recieve a F on this exam");
 
 	}
 
